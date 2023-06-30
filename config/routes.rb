30548e2658d5a28a9c033da45424d37df4817b31
Edem Topuzov <keltikan@gmail.com>
2023-06-30 14:32:13 +0300

    Remove legacy link redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'snippets#new'
 
-  get '/texts/:token', to: redirect('/s/%{token}')
   resources :s, only: %i[create show], param: :token, controller: :snippets, as: :snippets
 end
